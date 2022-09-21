f9f8f1d664e3f03420df7f2bf62877bcbd07fa72
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-09-21 23:16:23 +0900

    local DB 생성

--- app/src/main/java/org/techtown/nw_alarmer/localDB/MyWtList.kt
+++ app/src/main/java/org/techtown/nw_alarmer/localDB/MyWtList.kt
@@ -7,7 +7,10 @@ import org.techtown.nw_alarmer.WTData
 @Entity
 data class MyWtList(
 
-    var wtList : WTData
+    var wtTitle : String,
+    var wtImg : String,
+    var upImg : String,
+
 ){
     @PrimaryKey(autoGenerate = true) var id : Int = 0
 }
