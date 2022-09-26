a45d92ab806ef5983dcdfe61368f3c9b478438a1
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-09-26 20:12:57 +0900

    등록 액티비티 구현

---
+++ app/src/main/java/org/techtown/nw_alarmer/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.nw_alarmer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
